e1a2f39d2ddcdd15bddc13f0668f315504ca9c73
vinitasan979 <santhosv@uci.edu>
2020-07-22 07:39:29 -0700

    Current user class

--- TreehouseGuideBook/app/src/main/java/com/example/treehouseguidebook/SignUp.java
+++ TreehouseGuideBook/app/src/main/java/com/example/treehouseguidebook/SignUp.java
@@ -109,6 +109,7 @@ public class SignUp extends AppCompatActivity {
 
 
 
+
             }
         });
 
---
+++ TreehouseGuideBook/app/src/main/java/com/example/treehouseguidebook/Singleton.java
@@ -0,0 +1,14 @@
+package com.example.treehouseguidebook;
+
+public class Singleton {
+    public static  User existing_user= new User();
+    private Singleton(){}
+
+    public static User getExisting_user() {
+        return existing_user;
+    }
+
+    public static void setExisting_user(User existing_user) {
+        Singleton.existing_user = existing_user;
+    }
+}
